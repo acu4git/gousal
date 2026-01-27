50767736753eb12324cd3c36058c87f7994e39aa
Eishu Kawaguchi <sikewa-e.k@outlook.jp>
2026-01-27 15:02:00 +0900

    delete: Greet

--- app.go
+++ app.go
@@ -28,11 +28,6 @@ func (a *App) startup(ctx context.Context) {
 	a.ctx = ctx
 }
 
-// Greet returns a greeting for the given name
-func (a *App) Greet(name string) string {
-	return fmt.Sprintf("Hello %s, It's show time!", name)
-}
-
 func (a *App) SelectGoProject() (string, error) {
 	mainFiles := make([]string, 0)
 	for {
