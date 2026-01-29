abcc77388a76b69b309830e189d900577678dedd
Eishu Kawaguchi <sikewa-e.k@outlook.jp>
2026-01-30 02:11:17 +0900

    LoadingProvider, LoadingOverlay, AppRouter

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,55 +1,13 @@
-import { useState } from "react";
-import logo from "@/assets/images/logo-universal.png";
-import "@/App.css";
-import { SelectGoProject } from "#wailsjs/go/main/App";
-
-type Item = {
-  id: number;
-  name: string;
-};
+import { LoadingProvider } from "@/context/loading/LoadingContext";
+import { LoadingOverlay } from "@/context/loading/LoadingOverlay";
+import AppRouter from "@/router";
 
 const App = () => {
-  const [items, setItems] = useState<Item[]>([]);
-  const [selectedId, setSelectedId] = useState<number>(-1);
-
-  const selectGoProject = () => {
-    SelectGoProject().then(updateItems);
-  };
-
-  const updateItems = (files: string[]) => {
-    let newItems: Item[] = [];
-    files.map((file, idx) => {
-      const newcb: Item = {
-        id: idx,
-        name: file,
-      };
-      newItems.push(newcb);
-    });
-    setItems(newItems);
-  };
-
   return (
-    <div id="App">
-      <img src={logo} id="logo" alt="logo" />
-      <div className="flex flex-col items-baseline">
-        {items.map((item) => (
-          <label key={item.id}>
-            <input
-              type="radio"
-              name="go-project-files"
-              checked={item.id === selectedId}
-              onChange={() => setSelectedId(item.id)}
-            />
-            {item.name}
-          </label>
-        ))}
-      </div>
-      <div id="input" className="input-box">
-        <button className="hover:opacity-30" onClick={selectGoProject}>
-          Select Go Project
-        </button>
-      </div>
-    </div>
+    <LoadingProvider>
+      <LoadingOverlay />
+      <AppRouter />
+    </LoadingProvider>
   );
 };
 
