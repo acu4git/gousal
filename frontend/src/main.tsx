8fe52143e67d57c06dc72188b235be59782a177b
Eishu Kawaguchi <sikewa-e.k@outlook.jp>
2026-01-30 02:10:35 +0900

    AppRouter -> App

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import "@/style.css";
 import App from "@/App";
-import { HashRouter } from "react-router-dom";
 
 const container = document.getElementById("root");
 
@@ -10,8 +9,6 @@ const root = createRoot(container!);
 
 root.render(
   <React.StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
+    <App />
   </React.StrictMode>
 );
