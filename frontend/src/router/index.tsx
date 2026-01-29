11fc8e7f893b10385ebd7a6fcb453b02a2824e11
Eishu Kawaguchi <sikewa-e.k@outlook.jp>
2026-01-30 02:01:06 +0900

    AppRouter

---
+++ frontend/src/router/index.tsx
@@ -0,0 +1,19 @@
+import { useLoading } from "@/context/loading/LoadingContext";
+import Layout from "@/layout";
+import View from "@/pages/view";
+import { HashRouter, Route, Routes } from "react-router-dom";
+
+const AppRouter = () => {
+  const ctx = useLoading();
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<View />} />
+        </Route>
+      </Routes>
+    </HashRouter>
+  );
+};
+
+export default AppRouter;
