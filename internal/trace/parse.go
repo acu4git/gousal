acfa821e58418c98d00952dddbb9a5892c8f2d28
Eishu Kawaguchi <sikewa-e.k@outlook.jp>
2026-02-03 13:42:00 +0900

    ログを吐き出させる．modeの追加など

--- internal/trace/parse.go
+++ internal/trace/parse.go
@@ -2,15 +2,23 @@ package trace
 
 import (
 	"errors"
+	"fmt"
 	"io"
 	"os"
+	"path/filepath"
 	"strings"
 
 	xtrace "golang.org/x/exp/trace"
 )
 
+const (
+	MODE_FUNC_ENTER = "func-enter"
+	MODE_FUNC_EXIT  = "func-exit"
+)
+
 type StepInfo struct {
 	GID  int64
+	Mode string
 	Func string
 	File string
 }
@@ -29,6 +37,14 @@ func (sh StepHistory) hasFuncWithPrefix(args ...string) bool {
 }
 
 func Parse(traceFile string) ([]StepInfo, error) {
+	dir := filepath.Dir(traceFile)
+	logFile := filepath.Join(dir, "log.txt")
+	logf, err := os.Create(logFile)
+	if err != nil {
+		return nil, err
+	}
+	defer logf.Close()
+
 	r, cancel, err := traceReader(traceFile)
 	if err != nil {
 		return nil, err
@@ -52,20 +68,21 @@ func Parse(traceFile string) ([]StepInfo, error) {
 		if gid == -1 {
 			continue
 		}
-		stk := ev.Stack()
-		// スタックが存在しない場合もあるのでチェック"
-		if stk == xtrace.NoStack {
-			continue
-		}
+		// stk := ev.Stack()
+		// // スタックが存在しない場合もあるのでチェック"
+		// if stk == xtrace.NoStack {
+		// 	continue
+		// }
 
 		switch ev.Kind() {
 		case xtrace.EventLog:
-			funcDefID := ev.Log().Message
-			step, err := idToStepInfo(gid, funcDefID)
+			log := ev.Log()
+			step, err := funcDefIdToStepInfo(gid, log.Category, log.Message)
 			if err != nil {
 				return nil, err
 			}
 			stepHistory = append(stepHistory, step)
+			fmt.Fprintf(logf, "Goroutine %d\n\tmode: %s\n\tfuncDefID: %s\n", gid, log.Category, log.Message)
 		}
 	}
 
@@ -91,13 +108,14 @@ func traceReader(file string) (*xtrace.Reader, cancelFunc, error) {
 // funcDefIDは，"<project_root>/<rel>/<go_file>:<line>:<col>#<module>/<package>.<func>"のようなファイル情報と関数情報を'#'で区切る形式のID．
 //
 // 例外的に，mainパッケージのmain関数は"<project_root>/<rel>/<go_file>:<line>:<col>#main.main"と表現される．
-func idToStepInfo(gid xtrace.GoID, funcDefID string) (StepInfo, error) {
+func funcDefIdToStepInfo(gid xtrace.GoID, mode, funcDefID string) (StepInfo, error) {
 	fileInfo, funcInfo, ok := strings.Cut(funcDefID, "#")
 	if !ok {
 		return StepInfo{}, errors.New("failed to convert funcDefID to StepInfo: token '#' not found.")
 	}
 	return StepInfo{
 		GID:  int64(gid),
+		Mode: mode,
 		Func: funcInfo,
 		File: fileInfo,
 	}, nil
